38558bae83df5bf40fe17b6fc3b3fa01c3a99485
kaizel12 <134833137+kaizel12@users.noreply.github.com>
2024-08-18 06:22:36 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
-var pages = ['halaman1', 'halaman2', 'halaman3', 'halaman4', 'halaman5'];
+/*Author    : Wafa Rifqi Anafin*/
+/*Github    : https://github.com/wafarifki*/
+/*Facebook  : https://facebook.com/wafarifkianafin*/
+/*Instagram : https://instagram.com/wafarifki_*/
+/*Website   : https://wafarifki.tk*/
+
+var pages = new Array('halaman1', 'halaman2', 'halaman3', 'halaman4', 'halaman5');
 
 function klikMenu() {
-  document.querySelector('.dalemnya_kontener').classList.toggle('buka_menu');
+  document.getElementsByClassName('dalemnya_kontener')[0].classList.toggle('buka_menu');
 }
 
 function PindahKeHalaman(page) {
-  var dalemnya_kontener = document.querySelector('.dalemnya_kontener');
-  var sections = document.querySelectorAll('.dalemnya_halaman');
-  sections.forEach((section, index) => {
-    section.classList.remove('before', 'after');
-    if (index > page) {
-      section.classList.add('after');
+  var dalemnya_kontener = document.getElementsByClassName('dalemnya_kontener')[0];
+  var sections = document.getElementsByClassName('dalemnya_halaman');
+  for (i = 0; i < sections.length; i++) {
+    sections[i].classList.remove('before', 'after');
+    if (i > page) {
+      sections[i].classList.add('after');
     }
-  });
-  dalemnya_kontener.classList.remove('buka_menu', 'page-halaman1', 'page-halaman2', 'page-halaman3', 'page-halaman4', 'page-halaman5');
+  }
+  dalemnya_kontener.classList.remove('buka_menu', 'page-halaman1', 'page-halaman2');
   dalemnya_kontener.classList.add('page-' + pages[page]);
 }
-
-// Add event listener to the button to start the functionality
-document.getElementById('tombol').addEventListener('click', () => {
-  PindahKeHalaman(0); // You can change the index to switch to different pages
-});
