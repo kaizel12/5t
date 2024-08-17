5f5b9616f1aab45198e2f02657ef5607422b043c
kaizel12 <134833137+kaizel12@users.noreply.github.com>
2024-08-17 22:29:58 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ var pages = ['halaman1', 'halaman2', 'halaman3', 'halaman4', 'halaman5'];
 
 // Toggles the menu visibility
 function klikMenu() {
-  document.querySelector('.dalemnya_kontener').classList.toggle('buka_menu');
+  var dalemnya_kontener = document.querySelector('.dalemnya_kontener');
+  dalemnya_kontener.classList.toggle('buka_menu');
 }
 
 // Changes to the specified page
@@ -16,7 +17,6 @@ function PindahKeHalaman(page) {
   var dalemnya_kontener = document.querySelector('.dalemnya_kontener');
   var sections = document.querySelectorAll('.dalemnya_halaman');
 
-  // Update sections to reflect the new page
   sections.forEach((section, i) => {
     section.classList.remove('before', 'after');
     if (i > page) {
@@ -24,7 +24,6 @@ function PindahKeHalaman(page) {
     }
   });
 
-  // Manage class names for page transitions
   dalemnya_kontener.classList.remove('buka_menu', 'page-halaman1', 'page-halaman2', 'page-halaman3', 'page-halaman4', 'page-halaman5');
   dalemnya_kontener.classList.add('page-' + pages[page]);
 }
