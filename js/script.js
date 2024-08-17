7c1d4313c298b6b040a775d7a9a40742c5e8c388
kaizel12 <134833137+kaizel12@users.noreply.github.com>
2024-08-18 00:59:06 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,23 @@
-/*Author    : Rio Ludwig Alvino*/
-/*Github    : https://github.com/rioprojectx*/
-/*Facebook  : https://facebook.com/*/
-/*Instagram : https://instagram.com/san.rio31*/
-/*Website   : https://wafarifki.tk*/
-
-var pages = new Array('halaman1', 'halaman2', 'halaman3', 'halaman4', 'halaman5');
+var pages = ['halaman1', 'halaman2', 'halaman3', 'halaman4', 'halaman5'];
 
 function klikMenu() {
-  document.getElementsByClassName('dalemnya_kontener')[0].classList.toggle('buka_menu');
+  document.querySelector('.dalemnya_kontener').classList.toggle('buka_menu');
 }
 
 function PindahKeHalaman(page) {
-  var dalemnya_kontener = document.getElementsByClassName('dalemnya_kontener')[0];
-  var sections = document.getElementsByClassName('dalemnya_halaman');
-  
-  for (var i = 0; i < sections.length; i++) {
-    sections[i].classList.remove('before', 'after');
-    if (i < page) {
-      sections[i].classList.add('before');
-    } else if (i > page) {
-      sections[i].classList.add('after');
+  var dalemnya_kontener = document.querySelector('.dalemnya_kontener');
+  var sections = document.querySelectorAll('.dalemnya_halaman');
+  sections.forEach((section, index) => {
+    section.classList.remove('before', 'after');
+    if (index > page) {
+      section.classList.add('after');
     }
-  }
-  
-  dalemnya_kontener.classList.remove('buka_menu');
-  dalemnya_kontener.classList.remove('page-halaman1', 'page-halaman2', 'page-halaman3', 'page-halaman4', 'page-halaman5');
+  });
+  dalemnya_kontener.classList.remove('buka_menu', 'page-halaman1', 'page-halaman2', 'page-halaman3', 'page-halaman4', 'page-halaman5');
   dalemnya_kontener.classList.add('page-' + pages[page]);
 }
+
+// Add event listener to the button to start the functionality
+document.getElementById('tombol').addEventListener('click', () => {
+  PindahKeHalaman(0); // You can change the index to switch to different pages
+});
