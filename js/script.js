1aeb545cd91aeb7ea09f76263dd5cb28f0a6a496
kaizel12 <134833137+kaizel12@users.noreply.github.com>
2024-08-17 22:27:21 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,21 +4,27 @@
 /*Instagram : https://instagram.com/san.rio31*/
 /*Website   : https://wafarifki.tk*/
 
-var pages = new Array('halaman1', 'halaman2', 'halaman3', 'halaman4', 'halaman5');
+var pages = ['halaman1', 'halaman2', 'halaman3', 'halaman4', 'halaman5'];
 
+// Toggles the menu visibility
 function klikMenu() {
-  document.getElementsByClassName('dalemnya_kontener')[0].classList.toggle('buka_menu');
+  document.querySelector('.dalemnya_kontener').classList.toggle('buka_menu');
 }
 
+// Changes to the specified page
 function PindahKeHalaman(page) {
-  var dalemnya_kontener = document.getElementsByClassName('dalemnya_kontener')[0];
-  var sections = document.getElementsByClassName('dalemnya_halaman');
-  for (var i = 0; i < sections.length; i++) {
-    sections[i].classList.remove('before', 'after');
+  var dalemnya_kontener = document.querySelector('.dalemnya_kontener');
+  var sections = document.querySelectorAll('.dalemnya_halaman');
+
+  // Update sections to reflect the new page
+  sections.forEach((section, i) => {
+    section.classList.remove('before', 'after');
     if (i > page) {
-      sections[i].classList.add('after');
+      section.classList.add('after');
     }
-  }
-  dalemnya_kontener.classList.remove('buka_menu', 'page-halaman1', 'page-halaman2', 'page-halaman3', 'page-halaman4');
+  });
+
+  // Manage class names for page transitions
+  dalemnya_kontener.classList.remove('buka_menu', 'page-halaman1', 'page-halaman2', 'page-halaman3', 'page-halaman4', 'page-halaman5');
   dalemnya_kontener.classList.add('page-' + pages[page]);
 }
